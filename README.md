d5d7842f19541867b2faf202fc42f089dd5003c1
gui-bonnis <guibonnis@gmail.com>
2022-10-02 22:25:24 +0000

    Configuration for graalvm build

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # com-mymoney-transaction-api
+
+## to build gralvm package
+cd transaction-api
+./mvnw -DskipTests -Pnative clean package
+
+## to create docker image
+./mvnw -DskipTests clean package spring-boot:build-image
+
+# Install gralvm on linux
+https://www.graalvm.org/22.2/docs/getting-started/linux/
+## download tar
+mkdir ~/installs 
+cd installs
+curl https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.2.0/graalvm-ce-java17-linux-amd64-22.2.0.tar.gz ~/installs
+
+tar -xzf graalvm-ce-java17-linux-amd64-22.2.0.tar.gz
+  
+cd graalvm-ce-java17-22.2.0
+## Add to zshrc
+export PATH=$PWD/bin:$PATH
+export JAVA_HOME=$PWD
+
+
--- transaction.api/src/main/java/com/mymoney/transaction/api/Application.java
+++ transaction.api/src/main/java/com/mymoney/transaction/api/Application.java
@@ -2,6 +2,13 @@ package com.mymoney.transaction.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +17,33 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+  // @Bean
+  // ApplicationListener<AvaliabilityChangeEvent<?>> avalabilityChangeEventApplicationListener() {
+  //   return event -> System.out.println(event.getResolvableType() + " : " + event.getState());
+  // }
+  // @Bean
+  // ApplicationListener<ApplicationReadEvent> applicationReadEventApplicationListener(CustomerRepository customerRepository) {
+  //   return event -> Flux.just("gui", "gu", "gi").
+  //     .map(name -> new Customer(null, name))
+  //     .flatMap(customerRepository::save)
+  //     .subscribe(System.out::println);
+  //
+  // }
 }
+
+// @Controller
+// @ResponseBody
+// @RequiredArgsConstructor
+// class CustomerControlle {
+//
+//   private CustomerRepository customerRepository;
+//
+//   @GetMapping("/customers")
+//   Flux<Customer> get() {
+//     return this.customerRepository.findAll();
+//   }
+// }
+//
+// public interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {}
+//
+// public record Customer(@Id Integer id, String name) {}
