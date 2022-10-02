564b8dba036b11ed9cf216868a3dfb5b74a676d4
gui-bonnis <guibonnis@gmail.com>
2022-10-02 22:56:58 +0000

    Create CustomerController to test Container build

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ cd transaction-api
 
 # Install gralvm on linux
 https://www.graalvm.org/22.2/docs/getting-started/linux/
+
 ## download tar
 mkdir ~/installs 
 cd installs
@@ -16,9 +17,8 @@ curl https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.2.0/gr
 
 tar -xzf graalvm-ce-java17-linux-amd64-22.2.0.tar.gz
   
-cd graalvm-ce-java17-22.2.0
-## Add to zshrc
-export PATH=$PWD/bin:$PATH
-export JAVA_HOME=$PWD
+## Add to zshrc and bashrc
+export PATH=~/installs/graalvm-ce-java17-22.2.0/bin:$PATH
+export JAVA_HOME=~/installs/graalvm-ce-java17-22.2.0
 
 
--- transaction.api/src/main/java/com/mymoney/transaction/api/Application.java
+++ transaction.api/src/main/java/com/mymoney/transaction/api/Application.java
@@ -1,14 +1,18 @@
 package com.mymoney.transaction.api;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.availability.AvailabilityChangeEvent;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class Application {
@@ -17,33 +21,34 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-  // @Bean
-  // ApplicationListener<AvaliabilityChangeEvent<?>> avalabilityChangeEventApplicationListener() {
-  //   return event -> System.out.println(event.getResolvableType() + " : " + event.getState());
-  // }
-  // @Bean
-  // ApplicationListener<ApplicationReadEvent> applicationReadEventApplicationListener(CustomerRepository customerRepository) {
-  //   return event -> Flux.just("gui", "gu", "gi").
-  //     .map(name -> new Customer(null, name))
-  //     .flatMap(customerRepository::save)
-  //     .subscribe(System.out::println);
-  //
-  // }
+  @Bean
+  ApplicationListener<AvailabilityChangeEvent<?>> availabilityChangeEventApplicationListener() {
+    return event -> System.out.println(event.getResolvableType() + ":" + event.getState());
+  }
+
+  @Bean
+  ApplicationListener<ApplicationReadyEvent> applicationReadyEventApplicationListener(CustomerRepository customerRepository) {
+    return event -> Flux.just("gui", "gu", "gi")
+      .map(name -> new Customer(null, name))
+      .flatMap(customerRepository::save)
+      .subscribe(System.out::println);
+  }
+
 }
 
-// @Controller
-// @ResponseBody
-// @RequiredArgsConstructor
-// class CustomerControlle {
-//
-//   private CustomerRepository customerRepository;
-//
-//   @GetMapping("/customers")
-//   Flux<Customer> get() {
-//     return this.customerRepository.findAll();
-//   }
-// }
-//
-// public interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {}
-//
-// public record Customer(@Id Integer id, String name) {}
+@Controller
+@ResponseBody
+@RequiredArgsConstructor
+class CustomerControlle {
+
+  private final CustomerRepository customerRepository = null;
+
+  @GetMapping("/customers")
+  Flux<Customer> get() {
+    return this.customerRepository.findAll();
+  }
+}
+
+interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {}
+
+record Customer(@Id Integer id, String name) {}
